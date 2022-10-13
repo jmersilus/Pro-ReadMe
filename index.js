aca36665211c5855796e613899e2e30702f10e41
unknown <kingjoudel@icloud.com>
2022-10-13 15:33:39 -0400

    packages done

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application - 
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+
+// TODO: Create an array of questions for user input
+//const questions = [];
+promptUser = () => {}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
